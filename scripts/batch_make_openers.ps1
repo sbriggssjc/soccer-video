994f83ab462d980c2b5e6d6db6e585271ad6a9cb
sbriggssjc <sbriggssjc@gmail.com>
2025-10-09 10:13:24 -0500

    Call make_opener directly from batch script

--- scripts/batch_make_openers.ps1
+++ scripts/batch_make_openers.ps1
@@ -7,6 +7,9 @@ Import-Csv $RosterCsv | ForEach-Object {
     $name = $_.name
     $number = [int]$_.number
     $photo = $_.photo
-    pwsh -File (Join-Path $PSScriptRoot "make_opener.ps1") `
-        -PlayerName $name -PlayerNumber $number -PlayerPhoto $photo -OutDir $OutDir
+    & "$PSScriptRoot\make_opener.ps1" `
+        -PlayerName   $name `
+        -PlayerNumber $number `
+        -PlayerPhoto  $photo `
+        -OutDir       $OutDir
 }
