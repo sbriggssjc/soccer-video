cc69a3313e1f2d90af9d2d165a4a76d2a9c3fb0e
sbriggssjc <sbriggssjc@gmail.com>
2026-03-02 11:00:18 -0600

    GPT changes.

--- _cleanup.py
+++ _cleanup.py
@@ -1,4 +1,6 @@
 import glob, os
-for f in glob.glob(r"D:\Projects\soccer-video\_tmp\render_direct_002_result.txt"):
+for f in glob.glob(r"C:\Users\scott\Desktop\review_003.csv"):
+    os.remove(f)
+for f in glob.glob(r"C:\Users\scott\Desktop\filmstrip_003.png"):
     os.remove(f)
 print("DONE")
